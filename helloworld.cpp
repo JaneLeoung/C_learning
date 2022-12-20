c658dbd25bbb8ce4e222d16e4683a3e292f9c95f
JaneLeoung <121003084+JaneLeoung@users.noreply.github.com>
2022-12-20 16:12:36 +1100

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,6 +3,7 @@
 // the return type of main function is int
 int main()
 {
-	printf("hello world");
+	printf("hello world\n");
+	printf("%d\n", 100);// print out an integer, with format '%d'
 	return 0;
 }
