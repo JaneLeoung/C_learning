0ab81319041d5c12b4cd3acceb1ac9f39b95763d
JaneLeoung <121003084+JaneLeoung@users.noreply.github.com>
2022-12-20 16:17:42 +1100

    Add files via upload

---
+++ datatype.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(short int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+}
