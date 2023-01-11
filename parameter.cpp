2312959a266d2c73587a3877acadb77bfa015ad8
JaneLeoung <121003084+JaneLeoung@users.noreply.github.com>
2023-01-11 13:18:56 +1100

    Add files via upload

---
+++ parameter.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = { 0,1,2,3,4,5,6,7,8,9 };
+    int* p = arr;
+    int i = 0;
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d", *p);
+        p++;
+    }
+    return 0;
+}
