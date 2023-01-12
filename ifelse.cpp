37820b4dfce32e416480f4e6f901df7b8e16f9ea
JaneLeoung <121003084+JaneLeoung@users.noreply.github.com>
2023-01-13 00:53:11 +1100

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main()
+{
+    int yourinput;
+    printf("input your selection(1/0):");
+    scanf("%d", &yourinput);
+    if (yourinput == 1)
+        printf("your input is 1");
+    else
+        printf("your input is 0");
+    return 0;
+}
+
